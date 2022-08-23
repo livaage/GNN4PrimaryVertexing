3ba4bb492869a170751ab2e51b6dbf7cd003be12
Lucas-Borgna <lucas.borgna.16@ucl.ac.uk>
2022-08-23 17:47:47 +0100

    update graph building with radius

--- graph_building.py
+++ graph_building.py
@@ -1,65 +1,85 @@
+import argparse
+
 import pandas as pd
-import numpy as np 
-import time 
-import multiprocessing as mp 
+import numpy as np
+import time
+import multiprocessing as mp
 from functools import partial
-import os 
-
-#mc = pd.read_pickle('mc_25k.pkl')
-trk = pd.read_csv('untracked/trk_processed_25k.csv')
+import os
 
 
+def build_graph(event_number):
 
-n_events = trk['entry'].nunique() 
+    # select one event
+    event = trk[trk["entry"] == event_number]
 
-outdir = 'built_graphs/rebalanced/'
+    event["unique_trk_id"] = range(len(event))
 
-if not os.path.exists(outdir): 
-    os.makedirs(outdir) 
+    # 85% of samples are not primary vertex, repeat true vertices to rebalance
+    pv = event[event["is_pv"] == 1]
+    not_pv = event[event["is_pv"] == 0]
 
+    # balanced_df = pd.concat([pd.concat([pv] * 6), not_pv])
+    balanced_df = pd.concat([pv] + [not_pv])
 
-def build_graph(event_number): 
-    
-    #select one event 
-    event = trk[trk['entry'] == event_number] 
- 
-    event['unique_trk_id'] = range(len(event)) 
-   
-    # 85% of samples are not primary vertex, repeat true vertices to rebalance 
-    pv = event[event['is_pv']==1]
-    not_pv = event[event['is_pv']==0]
+    # fully connected edges
+    combos = balanced_df[["entry", "unique_trk_id", "trk_z0"]].merge(
+        balanced_df[["unique_trk_id", "entry", "trk_z0"]],
+        on="entry",
+        suffixes=("_1", "_2"),
+    )
 
-    balanced_df = pd.concat([pd.concat([pv]*6), not_pv])
-    
-    #fully connected edges 
-    combos = balanced_df[['entry', 'unique_trk_id']].merge(
-        balanced_df[['unique_trk_id', 'entry']], on='entry', suffixes=('_1', '_2'))
+    # remove node connection to itself
+    combos = combos[combos["unique_trk_id_1"] != combos["unique_trk_id_2"]]
 
-    #remove node connection to itself 
-    combos = combos[combos['unique_trk_id_1']!= combos['unique_trk_id_2']]
+    if radius is not None:
+        # remove nodes too far in z0 space
+        combos["abs_diff"] = np.abs(combos["trk_z0_1"] - combos["trk_z0_2"])
+        combos = combos[combos["abs_diff"] < radius]
 
-    #edge indices in correct format 
-    connections = np.stack((combos['unique_trk_id_1'].values, combos['unique_trk_id_2'].values))
-    X = balanced_df[['trk_pt', 'trk_eta', 'trk_phi', 'trk_z0']].values
-    
+    # edge indices in correct format
+    connections = np.stack(
+        (combos["unique_trk_id_1"].values, combos["unique_trk_id_2"].values)
+    )
+    X = balanced_df[["trk_pt", "trk_eta", "trk_phi", "trk_z0"]].values
 
-    np.savez(outdir+"graph_"+str(event_number), ** dict(x=X, edge_index=connections, y=balanced_df['is_pv'].values)) 
+    np.savez(
+        outdir + "graph_" + str(event_number),
+        **dict(x=X, edge_index=connections, y=balanced_df["is_pv"].values),
+    )
 
-    return 
-
-events = list(range(n_events)) 
-n_workers = 4
-
-t0 = time.time()
-with mp.Pool(processes=n_workers) as pool:
-    process_func = partial(build_graph)
-    pool.map(process_func, events)
-t1 = time.time()
-print("Finished in", t1-t0, "seconds")
+    return
 
 
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-r,", "--radius", type=float, default=None)
+    parser.add_argument("--outdir", type=str, default="built_graphs/")
+    parser.add_argument("--nevents", type=int, default=None)
+    parser.add_argument("--nworkers", type=int, default=4)
+    args = parser.parse_args()
 
+    # mc = pd.read_pickle('mc_25k.pkl')
+    # trk = pd.read_csv('untracked/trk_processed_25k.csv')
+    trk = pd.read_pickle("/media/lucas/QS/l1_nnt/trk_processed_25k.pkl")
+    trk = trk.reset_index()
+    if args.nevents is not None:
+        trk = trk.query(f"entry<{args.nevents}").copy()
 
+    n_events = trk["entry"].nunique()
 
-    
+    outdir = args.outdir
 
+    if not os.path.exists(outdir):
+        os.makedirs(outdir)
+
+    events = list(range(n_events))
+    n_workers = args.nworkers
+    radius = args.radius
+
+    t0 = time.time()
+    with mp.Pool(processes=n_workers) as pool:
+        process_func = partial(build_graph)
+        pool.map(process_func, events)
+    t1 = time.time()
+    print("Finished in", t1 - t0, "seconds")
--- models/gcn.py
+++ models/gcn.py
@@ -1,24 +1,57 @@
 import torch
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv
+from torch_geometric.nn import GCNConv, GATConv
+
 
 class GCN(torch.nn.Module):
     def __init__(self):
         super().__init__()
         self.conv1 = GCNConv(4, 64)
+        self.conv15 = GCNConv(64, 64)
         self.conv2 = GCNConv(64, 1)
 
-#        self.lin1 = Linear(data.num_features, hidden_channels)
-#        self.lin2 = Linear(hidden_channels, dataset.num_classes)
+    #        self.lin1 = Linear(data.num_features, hidden_channels)
+    #        self.lin2 = Linear(hidden_channels, dataset.num_classes)
 
     def forward(self, data):
         x, edge_index = data.x, data.edge_index
-        
+
         x = self.conv1(x, edge_index)
         x = F.relu(x)
         x = F.dropout(x, training=self.training)
         x = self.conv2(x, edge_index)
-      
-        final = torch.cat([x], dim=1) 
-        return F.sigmoid(final)
 
+        final = torch.cat([x], dim=1)
+        return torch.sigmoid(final)
+
+
+class GAT(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+        self.hidden = 8
+        self.in_head = 8
+        self.out_head = 1
+        self.num_features = 4
+        self.num_classes = 1
+        self.conv1 = GATConv(
+            self.num_features, self.hidden, heads=self.in_head, dropout=0.6
+        )
+
+        self.conv2 = GATConv(
+            self.hidden * self.in_head,
+            self.num_classes,
+            concat=False,
+            heads=self.out_head,
+            dropout=0.6,
+        )
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+        x = F.dropout(x, p=0.6, training=self.training)
+        x = self.conv1(x, edge_index)
+        x = F.elu(x)
+        x = F.dropout(x, p=0.6, training=self.training)
+        x = self.conv2(x, edge_index)
+        final = torch.cat([x], dim=1)
+        return torch.sigmoid(final)
